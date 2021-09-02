ad1309a5410c88e2d822582c12bc0cdd787b6dde
Kiril95 <tolevkiril@gmail.com>
2021-09-02 23:26:46 +0300

    Additional Solutions

---
+++ Stacks and Queues - Exercises/Balanced Parenthesis/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Balanced_Parenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<string> brackets = new Stack<string>();
+
+            bool isTrue = true;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string current = input[i].ToString();
+
+                if (current == "{" || current == "(" || current == "[")
+                {
+                    brackets.Push(current);
+                }
+
+                else if (current == ")" || current == "}" || current == "]")
+                {
+                    if (brackets.Count <= 0)
+                    {
+                        isTrue = false;
+                        break;
+                    }
+                    string previous = brackets.Peek();
+
+                    if (current == "}" && previous == "{" || current == ")" && previous == "("
+                                                          || current == "]" && previous == "[")
+                    {
+                        brackets.Pop();
+                    }
+                    else
+                    {
+                        isTrue = false;
+                        break;
+                    }
+
+                }
+            }
+            //  {[()]}
+            //  {[(])}
+            //  { {[[(())]]} }
+
+            if (isTrue)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+        }
+    }
+}
---
+++ Stacks and Queues - Exercises/Truck Tour/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Truck_Tour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pumps = int.Parse(Console.ReadLine());
+            //Queue<int> petrol = new Queue<int>();
+            //Queue<int> distance = new Queue<int>();
+
+            double current = 0;
+            double result = 0;
+            for (long i = 0; i < pumps; i++)
+            {
+                int[] array = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                int amount = array[0];
+                int distance = array[1];
+
+                current += amount;
+
+                if (current >= distance)
+                {
+                    current -= distance;
+                }
+                else
+                {
+                    result = i + 1;
+                    current = 0;
+                }
+
+            }
+
+            //GC.Collect();
+            Console.WriteLine(result);
+            
+        }
+    }
+}
