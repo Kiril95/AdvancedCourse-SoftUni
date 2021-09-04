29307e0168e7935600584171188d618f68dd1ebd
Kiril95 <tolevkiril@gmail.com>
2021-09-04 20:10:51 +0300

    Added last few exercises

---
+++ Stacks and Queues - Exercises/Cups and Bottles/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Security.Cryptography;
+
+namespace Cups_and_Bottles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[] array2 = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> cups = new Queue<int>(array1);
+            Stack<int> bottles = new Stack<int>(array2);
+            //Stack<int> wastedWaterStack = new Stack<int>(array2);
+            int wasted = 0;
+
+            while (bottles.Any() && cups.Any())
+            {
+                int currentBottle = bottles.Peek();
+                int currentCup = cups.Peek();
+
+                if (currentBottle >= currentCup)
+                {
+                    wasted += currentBottle - currentCup;
+                    int temp = currentBottle - currentCup;
+                    bottles.Pop();
+                    cups.Dequeue();
+
+                }
+
+                else if (currentCup > currentBottle)
+                {
+                    int temp = currentCup - currentBottle;
+                    bottles.Pop();
+
+                    while (temp > 0 && bottles.Count != 0)
+                    {
+                       
+                        int next = bottles.Peek();
+
+                        if (next > temp)
+                        {
+                            wasted += next - temp;
+                            temp -= next;
+                        }
+                        else
+                        {
+                            temp -= next;
+                        }
+
+                        bottles.Pop();
+                    }
+
+                    cups.Dequeue();
+                }
+
+            }
+            if (bottles.Any())
+            {
+                Console.WriteLine($"Bottles: {string.Join(" ", bottles)}");
+                Console.WriteLine($"Wasted litters of water: {wasted}");
+            }
+
+            else if (cups.Any())
+            {
+                Console.WriteLine($"Cups: {string.Join(" ", cups)}");
+                Console.WriteLine($"Wasted litters of water: {wasted}");
+            }
+        }
+    }
+}
---
+++ Stacks and Queues - Exercises/Key Revolver/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Key_Revolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bulletPr = int.Parse(Console.ReadLine());
+            int gunBarrel = int.Parse(Console.ReadLine());
+            int[] array1 = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[] array2 = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int secretPr = int.Parse(Console.ReadLine());
+            int ammo = 0;
+            int money = 0;
+            Stack<int> bullets = new Stack<int>(array1);
+            Queue<int> locks = new Queue<int>(array2);
+
+            while (bullets.Any() && locks.Any())
+            {
+
+                if (bullets.Peek() <= locks.Peek())
+                {
+                    Console.WriteLine("Bang!");
+                    locks.Dequeue();
+                    bullets.Pop();
+                    secretPr -= bulletPr;
+                }
+                else
+                {
+                    Console.WriteLine("Ping!");
+                    bullets.Pop();
+                    secretPr -= bulletPr;
+                }
+
+                ammo++;
+                if (bullets.Any() && gunBarrel == ammo)
+                {
+                    Console.WriteLine("Reloading!");
+                    ammo = 0;
+                }
+                money++;
+            }
+
+            GC.Collect();
+            if (!locks.Any())
+            {
+                Console.WriteLine($"{bullets.Count()} bullets left. Earned ${secretPr}");
+            }
+            else
+            {
+                Console.WriteLine($"Couldn't get through. Locks left: {locks.Count()}");
+            }
+        }
+    }
+}
