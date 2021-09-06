6d17b67b489149ac2ebdb8c78820af65b77d4a54
Kiril95 <tolevkiril@gmail.com>
2021-09-06 18:31:40 +0300

    More Solutions

---
+++ Multidimensional Arrays - Lab/Square With Maximum Sum/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace Square_With_Maximum_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizeMatrix = ReadIntArray();
+            int[,] matrix = new int[sizeMatrix[0], sizeMatrix[1]];
+            int max = int.MinValue;
+            int[,] subMatrix = new int[2, 2];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] array = ReadIntArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = array[col];
+                }
+            }
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    int sum = matrix[row, col] + matrix[row + 1, col] 
+                        + matrix[row, col + 1] + matrix[row + 1, col + 1];
+
+                    if (sum > max)
+                    {
+                        max = sum;
+                        subMatrix[0, 0] = matrix[row, col];
+                        subMatrix[0, 1] = matrix[row, col + 1];
+                        subMatrix[1, 0] = matrix[row + 1, col];
+                        subMatrix[1, 1] = matrix[row + 1, col + 1];
+
+                    }
+                }
+            }
+            Console.WriteLine($"{subMatrix[0, 0]} {subMatrix[0, 1]}");
+            Console.WriteLine($"{subMatrix[1, 0]} {subMatrix[1, 1]}");
+            Console.WriteLine(max);
+
+        }
+        private static int[] ReadIntArray()
+        {
+            return Console.ReadLine()
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+        }
+    }
+}
---
+++ Multidimensional Arrays - Lab/Symbol in Matrix/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace Symbol_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[size, size];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string input = Console.ReadLine();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+
+            char symbol = char.Parse(Console.ReadLine());
+
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size; col++)
+                {
+                    if (matrix[row, col] == symbol)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        return;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{symbol} does not occur in the matrix");
+
+        }
+    }
+}
