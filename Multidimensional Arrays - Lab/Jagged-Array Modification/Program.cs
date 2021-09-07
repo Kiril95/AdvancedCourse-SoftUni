c68a8d86dcecbab1ed6268699c47a3d9ca7bf201
Kiril95 <tolevkiril@gmail.com>
2021-09-07 23:47:07 +0300

    Added last exercises

---
+++ Multidimensional Arrays - Lab/Jagged-Array Modification/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+
+namespace Jagged_Array_Modification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[][] jaggy = new int[size][];
+
+            for (int i = 0; i < jaggy.Length; i++)
+            {
+                int[] arr = ReadIntArray();
+                jaggy[i] = arr;
+            }
+
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                string[] operations = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                int rowIdx = int.Parse(operations[1]);
+                int colIdx = int.Parse(operations[2]);
+                int value = int.Parse(operations[3]);
+
+                if (operations[0] == "Add" &&
+                    rowIdx >= 0 && rowIdx < jaggy.Length &&
+                    colIdx >= 0 && colIdx < jaggy[rowIdx].Length)
+                {
+                    jaggy[rowIdx][colIdx] += value;
+                }
+
+                else if (operations[0] == "Subtract" &&
+                         rowIdx >= 0 && rowIdx < jaggy.Length &&
+                         colIdx >= 0 && colIdx < jaggy[rowIdx].Length)
+                {
+                    jaggy[rowIdx][colIdx] -= value;
+                }
+
+                else
+                {
+                    Console.WriteLine("Invalid coordinates");
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var item in jaggy)
+            {
+                Console.WriteLine(string.Join(" " ,item));
+            }
+
+        }
+        private static int[] ReadIntArray()
+        {
+            return Console.ReadLine()
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+        }
+    }
+}
---
+++ Multidimensional Arrays - Lab/Pascal Triangle/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Pascal_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long size = long.Parse(Console.ReadLine());
+
+            long[][] jaggy = new long[size][];
+
+            for (int i = 0; i < jaggy.Length; i++)
+            {
+                long[] arr = new long[i + 1];
+                arr[0] = 1;
+                arr[i] = 1;
+
+                for (int j = 1; j < i; j++)
+                {
+                    arr[j] = jaggy[i - 1][j] + jaggy[i - 1][j - 1];
+                }
+
+                jaggy[i] = arr;
+            }
+
+            foreach (var item in jaggy)
+            {
+                Console.WriteLine(string.Join(' ', item));
+            }
+
+        }
+    }
+}
