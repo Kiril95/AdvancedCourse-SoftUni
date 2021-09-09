02717cf02e57970df876d4e7925b0b493160f429
Kiril95 <tolevkiril@gmail.com>
2021-09-09 12:32:36 +0300

    Additional Solutions

---
+++ Multidimensional Arrays - Exercise/Jagged Array Manipulator/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+
+namespace Jagged_Array_Manipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            double[][] jaggy = new double[rows][];
+
+            for (int i = 0; i < jaggy.Length; i++)
+            {
+                jaggy[i] = ReadDoubleArray();
+            }
+
+            for (int row = 0; row < jaggy.Length - 1; row++)
+            {
+                if (jaggy[row].Length == jaggy[row + 1].Length)
+                {
+                    for (int col = 0; col < jaggy[row].Length; col++)
+                    {
+                        jaggy[row][col] *= 2;
+                        jaggy[row + 1][col] *= 2;
+
+                    }
+                }
+                else
+                {
+                    for (int col = 0; col < jaggy[row].Length; col++)
+                    {
+                        jaggy[row][col] /= 2;
+                    }
+
+                    for (int col = 0; col < jaggy[row + 1].Length; col++)
+                    {
+                        jaggy[row + 1][col] /= 2;
+                    }
+                }
+
+            }
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                string[] operations = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                int rowIdx = int.Parse(operations[1]);
+                int colIdx = int.Parse(operations[2]);
+                double value = double.Parse(operations[3]);
+
+                if (operations[0] == "Add" && Validation(jaggy, rowIdx, colIdx))
+                {
+                    jaggy[rowIdx][colIdx] += value;
+                }
+
+                else if (operations[0] == "Subtract" && Validation(jaggy, rowIdx, colIdx))
+                {
+                    jaggy[rowIdx][colIdx] -= value;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var item in jaggy)
+            {
+                Console.WriteLine(string.Join(" ", item));
+            }
+
+        }
+        public static double[] ReadDoubleArray()
+        {
+            return Console.ReadLine()
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+        }
+        public static bool Validation(double[][] jaggy, int rowIdx, int colIdx)
+        {
+            if (rowIdx >= 0 && rowIdx < jaggy.Length &&
+                colIdx >= 0 && colIdx < jaggy[rowIdx].Length)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Multidimensional Arrays - Exercise/Snake Moves/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+namespace Snake_Moves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizeMatrix = ReadIntArray();
+            char[,] matrix = new char[sizeMatrix[0], sizeMatrix[1]];
+            string snake = Console.ReadLine();
+            //string tempSnake = snake;
+            int count = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                if (row % 2 == 0)
+                {
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+                        if (snake.Length == count)
+                        {
+                            count = 0;
+                        }
+
+                        matrix[row, col] = snake[count++];
+                        //tempSnake = tempSnake.Remove(0, 1);
+                    }
+                }
+                else
+                {
+                    for (int i = matrix.GetLength(1) - 1; i >= 0; i--)
+                    {
+                        if (snake.Length == count)
+                        {
+                            count = 0;
+                        }
+
+                        matrix[row, i] = snake[count++];
+                        //tempSnake = tempSnake.Remove(0, 1);
+                    }
+                }
+
+            }
+            PrintMatrix(sizeMatrix, matrix);
+
+        }
+        public static int[] ReadIntArray()
+        {
+            return Console.ReadLine()
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+        }
+        public static void PrintMatrix(int[] matrixSizes, char[,] matrix)
+        {
+            for (int row = 0; row < matrixSizes[0]; row++)
+            {
+                for (int col = 0; col < matrixSizes[1]; col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
