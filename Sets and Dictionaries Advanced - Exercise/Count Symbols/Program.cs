b24ad3965fdb3b77601afa2bd6df38522a3511da
Kiril95 <tolevkiril@gmail.com>
2021-09-12 23:41:53 +0300

    Adding new solutions

---
+++ Sets and Dictionaries Advanced - Exercise/Count Symbols/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            SortedDictionary<char, int> result = new SortedDictionary<char, int>();
+
+            foreach (var item in line)
+            {
+                if (result.ContainsKey(item))
+                {
+                    result[item]++;
+                }
+                else
+                {
+                    result.Add(item, 1);
+                }
+            }
+
+            foreach (var symbol in result)
+            {
+                Console.WriteLine($"{symbol.Key}: {symbol.Value} time/s");
+            }
+        }
+    }
+}
---
+++ Sets and Dictionaries Advanced - Exercise/Wardrobe/Program.cs
@@ -0,0 +1,60 @@
+﻿ using System;
+ using System.Collections.Generic;
+
+ namespace Wardrobe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int times = int.Parse(Console.ReadLine());
+            Dictionary<string, Dictionary<string, int>> wardrobe 
+                = new Dictionary<string, Dictionary<string, int>>();
+
+            for (int i = 0; i < times; i++)
+            {
+                string[] first = Console.ReadLine().Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+                string color = first[0];
+                string[] second = first[1].Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var item in second)
+                {
+                    if (wardrobe.ContainsKey(color))
+                    {
+                        if (wardrobe[color].ContainsKey(item))
+                        {
+                            wardrobe[color][item]++;
+                        }
+                        else
+                        {
+                            wardrobe[color].Add(item, 1);
+                        }
+                    }
+                    else
+                    {
+                        wardrobe.Add(color, new Dictionary<string, int>{{ item, 1 }});
+                    }
+                }
+            }
+            string[] searched = Console.ReadLine().Split();
+            string searchedColor = searched[0];
+            string searchedType = searched[1];
+
+            foreach (var item in wardrobe)
+            {
+                Console.WriteLine($"{item.Key} clothes:");
+
+                foreach (var (key, value) in item.Value)
+                {
+                    if (item.Key == searchedColor && key == searchedType)
+                    {
+                        Console.WriteLine($"* {key} - {value} (found!)");
+                        continue;
+                    }
+
+                    Console.WriteLine($"* {key} - {value}");
+                }
+            }
+        }
+    }
+}
