66a58df06f42b4fe6ee107fe3cc43742c9484a40
Kiril95 <tolevkiril@gmail.com>
2021-09-13 22:08:22 +0300

    Added last exercises

---
+++ Sets and Dictionaries Advanced - Exercise/Ranking/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ranking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> langWithPass = new Dictionary<string, string>();
+            Dictionary<string, Dictionary<string, int>> database =
+                                    new Dictionary<string, Dictionary<string, int>>();
+            string command = Console.ReadLine();
+
+            while (command != "end of contests")
+            {
+                string[] operations = command.Split(":");
+                string language = operations[0];
+                string pass = operations[1];
+
+                if (!langWithPass.ContainsKey(language))
+                {
+                    langWithPass.Add(language, pass);
+                }
+                command = Console.ReadLine();
+
+            }
+            command = Console.ReadLine();
+
+            while (command != "end of submissions")
+            {
+                string[] operations2 = command.Split("=>");
+                string language = operations2[0];
+                string pass = operations2[1];
+                string user = operations2[2];
+                int points = int.Parse(operations2[3]);
+
+                if (langWithPass.ContainsKey(language)
+                   && langWithPass[language] == pass)
+                {
+                    if (!database.ContainsKey(user))
+                    {
+                        database.Add(user, new Dictionary<string, int> { { language, points } });
+                    }
+
+                    if (database[user].ContainsKey(language))
+                    {
+                        if (database[user][language] < points)
+                        {
+                            database[user][language] = points;
+                        }
+                    }
+
+                    else
+                    {
+                        database[user].Add(language, points);
+                    }
+                }
+                command = Console.ReadLine();
+
+            }
+            // var max = main.Values.Max();
+            int best = 0;
+            string bestUser = string.Empty;
+            foreach (var student in database)
+            {
+                int sum = 0;
+                foreach (var contest in student.Value)
+                {
+                    sum += contest.Value;
+                }
+                if (sum > best)
+                {
+                    bestUser = student.Key;
+                    best = sum;
+                }
+            }
+
+            Console.WriteLine($"Best candidate is {bestUser} with total {best} points.");
+            Console.WriteLine("Ranking:");
+
+            foreach (var item in database.OrderBy(x => x.Key))
+            {
+                Console.WriteLine(item.Key);
+
+                foreach (var kvp in item.Value.OrderByDescending(x => x.Value))
+                {
+                    Console.WriteLine($"#  {kvp.Key} -> {kvp.Value}");
+                }
+            }
+        }
+    }
+}
---
+++ Sets and Dictionaries Advanced - Exercise/The V-Logger/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace The_V_Logger
+{
+    class Program
+    {
+        public class Statistics
+        {
+            public SortedSet<string> Following { get; set; }
+            public SortedSet<string> Followers { get; set; }
+
+            public Statistics(SortedSet<string> following, SortedSet<string> followers)
+            {
+                Following = following;
+                Followers = followers;
+            }
+        }
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            Dictionary<string, Statistics> register = new Dictionary<string, Statistics>();
+
+            while (command != "Statistics")
+            {
+                string[] operations = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string vlogger = operations[0];
+
+                if (operations[1] == "joined")
+                {
+                    if (!register.ContainsKey(vlogger))
+                    {
+                        register.Add(vlogger,
+                        new Statistics(new SortedSet<string>(), new SortedSet<string>()));
+                    }
+                }
+                else if (operations[1] == "followed")
+                {
+                    string secondVl = operations[2];
+
+                    if (register.ContainsKey(vlogger) && register.ContainsKey(secondVl)
+                              && vlogger != secondVl)
+                    {
+                        if (!register[vlogger].Following.Contains(secondVl))
+                        {
+                            register[vlogger].Following.Add(secondVl);
+                            register[secondVl].Followers.Add(vlogger);
+                        }
+                    }
+                }
+                command = Console.ReadLine();
+
+            }
+            int place = 1;
+            Console.WriteLine($"The V-Logger has a total of {register.Count} vloggers in its logs.");
+
+            foreach (var item in register
+                .OrderByDescending(x => x.Value.Followers.Count)
+                .ThenBy(x => x.Value.Following.Count))
+            {
+                if (place == 1 && item.Value.Followers.Count > 0)
+                {
+                    Console.WriteLine($"{place}. {item.Key} : {item.Value.Followers.Count} " +
+                                      $"followers, {item.Value.Following.Count} following");
+
+                    foreach (var person in item.Value.Followers)
+                    {
+                        Console.WriteLine($"*  {person}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"{place}. {item.Key} : {item.Value.Followers.Count} " +
+                                      $"followers, {item.Value.Following.Count} following");
+                }
+                place++; 
+            }
+        }
+    }
+}
