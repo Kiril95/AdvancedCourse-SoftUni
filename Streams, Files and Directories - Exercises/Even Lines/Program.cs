c07c7cf8718e4ed1309a3f1524014dbef49d22de
Kiril95 <tolevkiril@gmail.com>
2021-09-18 23:50:58 +0300

    First exercises

---
+++ Streams, Files and Directories - Exercises/Even Lines/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Even_Lines
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string[] text = await File.ReadAllLinesAsync("text.txt");
+            int count = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                string current = text[i];
+
+                if (count % 2 == 0)
+                {
+                    string[] reversed = current.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                        .Reverse()
+                        .ToArray();
+                    string joined = string.Join(" ", reversed);
+                    string replaced = Regex.Replace(joined, @"[^\w\d\s]", "@");
+
+                    Console.WriteLine(replaced);
+                }
+                count++;
+
+            }
+        }
+    }
+}
---
+++ Streams, Files and Directories - Exercises/Line Numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Line_Numbers
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string[] text = await File.ReadAllLinesAsync("text.txt");
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                string current = text[i];
+                int letters = Regex.Matches(current, @"[A-Za-z]")
+                    .OfType<Match>()
+                    .Select(x => x.Value)
+                    .ToArray()
+                    .Count();
+
+                int symbols = Regex.Matches(current, @"[^\w\d\s]")
+                    .OfType<Match>()
+                    .Select(x => x.Value)
+                    .ToArray()
+                    .Count();
+
+                string forPrinting = $"Line {i + 1}:{current}({letters})({symbols}){Environment.NewLine}";
+                await File.AppendAllTextAsync("Output.txt", forPrinting);
+
+            }
+        }
+    }
+}
+
