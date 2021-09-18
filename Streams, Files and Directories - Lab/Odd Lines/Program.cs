c7b59c8a7285f6548cace3220a1a4837169ccb14
Kiril95 <tolevkiril@gmail.com>
2021-09-18 09:50:18 +0300

    Testing

---
+++ Streams, Files and Directories - Lab/Odd Lines/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Odd_Lines
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (StreamWriter writer = new StreamWriter("Output.txt"))
+            {
+                using (StreamReader reader = new StreamReader("Input.txt"))
+                {
+                    int current = 0;
+                    string text = await reader.ReadLineAsync();
+
+                    while (text != null)
+                    {
+                        if (current % 2 != 0)
+                        {
+                            await writer.WriteLineAsync(text);
+                        }
+
+                        current++;
+                        text = await reader.ReadLineAsync();
+                    }
+
+                }
+            }
+
+        }
+    }
+}
