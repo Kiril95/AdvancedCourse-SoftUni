9b86a52ddd1cd80870e47fcb08d52edeb0f8d099
Kiril95 <tolevkiril@gmail.com>
2021-10-03 18:11:55 +0300

    Added last exercise

---
+++ Iterators and Comparators - Exercise/EqualityLogic/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace EqualityLogic
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            if (this.Name.CompareTo(other.Name) != 0)
+            {
+                return this.Name.CompareTo(other.Name);
+            }
+
+            if (this.Age.CompareTo(other.Age) != 0)
+            {
+                return this.Age.CompareTo(other.Age);
+            }
+            return 0;
+        }
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() + Age.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Person;
+
+            if (other == null)
+            {
+                return false;
+            }
+
+            return this.Age == other.Age && this.Name == other.Name;
+        }
+    }
+}
---
+++ Iterators and Comparators - Exercise/EqualityLogic/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EqualityLogic
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<Person> sortSet = new SortedSet<Person>();
+            HashSet<Person> hSet = new HashSet<Person>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                Person newPerson = new Person(name, age);
+
+                sortSet.Add(newPerson);
+                hSet.Add(newPerson);
+            }
+
+            Console.WriteLine(sortSet.Count);
+            Console.WriteLine(hSet.Count);
+        }
+    }
+}
