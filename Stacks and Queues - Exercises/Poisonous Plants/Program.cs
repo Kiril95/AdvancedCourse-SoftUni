2e01add9ae9e213f52e64e8f57501ed23cd5b07a
Kiril95 <tolevkiril@gmail.com>
2021-10-04 21:03:12 +0300

    Bonus exercises

---
+++ Stacks and Queues - Exercises/Poisonous Plants/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Poisonous_Plants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> plants = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            Stack<int> plantsIndexes = new Stack<int>();
+
+            int daysCount = 0;
+            int deathCount = 1;
+
+            while (plants.Count > 0)
+            {
+                if (deathCount <= 0)
+                {
+                    break;
+                }
+                deathCount = 0;
+
+                for (int i = 0; i < n - 1; i++)
+                {
+                    if (plants[i + 1] > plants[i])
+                    {
+                        plantsIndexes.Push(i + 1);
+
+                        deathCount++;
+                    }
+                }
+
+                for (int i = 0; i < deathCount; i++)
+                {
+                    plants.RemoveAt(plantsIndexes.Pop());
+                }
+
+                n = plants.Count;
+                daysCount++;
+            }
+
+            Console.WriteLine(daysCount - 1);
+        }
+    }
+}
---
+++ Stacks and Queues - Exercises/Stack Fibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            Stack<long> stack = new Stack<long>();
+            stack.Push(0);
+            stack.Push(1);
+
+            for (int i = 1; i < num; i++)
+            {
+                long first = stack.Pop();
+                long second = stack.Pop();
+
+                stack.Push(first);
+                stack.Push(first + second);
+            }
+
+            Console.WriteLine(stack.Peek());
+        }
+    }
+}
